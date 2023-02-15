79d7cddd2f7588dc5d9b4cdc9503708613d9c8f2
NepalJohn21 <124006434+NepalJohn21@users.noreply.github.com>
2023-02-15 11:10:41 -0300

    Create ga-html.js

---
+++ ga-html.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("head")[0].innerHTML += `<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QTRX1F54VP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QTRX1F54VP');
+</script>`
